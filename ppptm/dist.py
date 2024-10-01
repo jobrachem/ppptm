864f9e541e99d10f887bcc56469869d76562741e
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-10-01 15:17:41 +0200

    Update dist.py

--- ppptm/dist.py
+++ ppptm/dist.py
@@ -1,3 +1,4 @@
+import jax.numpy as jnp
 import tensorflow_probability.substrates.jax.distributions as tfd
 from tensorflow_probability.substrates.jax import tf2jax as tf
 
@@ -8,6 +9,7 @@ class CustomGEV(tfd.GeneralizedExtremeValue):
         loc,
         scale,
         concentration,
+        support_penalty: float = 1e4,
         validate_args=False,
         allow_nan_stats=True,
         name="CustomGEV",
@@ -23,15 +25,42 @@ class CustomGEV(tfd.GeneralizedExtremeValue):
 
         # Create a Gumbel distribution (used if concentration == 0)
         self.gumbel_dist = tfd.Gumbel(loc=loc, scale=scale)
+        self.support_penalty = support_penalty
+
+    @classmethod
+    def _parameter_properties(cls, dtype, num_classes=None):
+        # Explicitly using the _parameter_properties classmethod of the parent class,
+        # because otherwise TFP will raise a warning. Since the parameters are exactly
+        # the same as for the parent class, this is appropriate.
+        return super()._parameter_properties(dtype, num_classes)
 
     def log_prob(self, value):
-        gev_log_prob = super().log_prob(value)
-        gumbel_log_prob = self.gumbel_dist.log_prob(value)
+        # standardized value for testing support
+        # y <= 0 means the value is outside the support of the distribution
+        y = 1 + self.concentration * ((value - self.loc) / self.scale)
 
-        log_prob = tf.where(
-            tf.equal(self.concentration, 0.0), gumbel_log_prob, gev_log_prob
+        # Using the inner-outer jnp.where pattern to obtain working gradients
+        # see here: https://jax.readthedocs.io/en/latest/faq.html#gradients-contain-nan-where-using-where # noqa
+        # The values created here are only used to avoid NaNs;
+        # they will be deselected by the final call to jnp.where.
+        x_gumbel_safe = 0.5 * self.scale + self.loc
+        x_gev_safe = self.scale * (0.5 - 1.0) / self.concentration + self.loc
+
+        safe_value = jnp.where(
+            y <= 0,
+            jnp.where(tf.equal(self.concentration, 0.0), x_gumbel_safe, x_gev_safe),
+            value,
         )
 
-        log_prob = tf.where(tf.is_inf(log_prob), tf.constant(-1e6), log_prob)
+        # Explicitly fall back to Gumbel distribution for concentration == 0
+        # I think the GEV gives the wrong support in this case, see this issue:
+        # https://github.com/tensorflow/probability/issues/1839
+        log_prob = jnp.where(
+            tf.equal(self.concentration, 0.0),
+            self.gumbel_dist.log_prob(safe_value),
+            super().log_prob(safe_value),
+        )
 
-        return log_prob
+        # returns a penalized log prob, with the penalty getting stronger for stronger
+        # deviations from the needed support.
+        return jnp.where(y <= 0, (y - 0.1) * self.support_penalty, log_prob)
