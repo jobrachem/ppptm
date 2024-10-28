99d9d56ae736a3720261bfaad0dc5df9e61ed498
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-10-28 14:49:33 +0100

    Update dist.py

--- ppptm/dist.py
+++ ppptm/dist.py
@@ -4,6 +4,23 @@ from tensorflow_probability.substrates.jax import tf2jax as tf
 
 
 class CustomGEV(tfd.GeneralizedExtremeValue):
+    """
+    Parameters
+    ----------
+    support_penalty
+        Penalty value to use instead of ``NaN`` log probabilities for values outside \
+        of the distribution's support.
+    extended_support_boundary
+        If this is ``extended_support_boundary > 0``, the log probabilities for values \
+        that are inside the support by ``extended_support_boundary`` will also be \
+        replaced with the penalty value. This can increase numerical stability for \
+        evaluating the log probability on values that are *just barely* inside the \
+        support.
+    scale_penalty
+        If ``True`` the penalty is scaled with the number of support violations, such \
+        that the total penalty pseudo-log-probabilities sum to ``support_penalty``.
+    """
+
     def __init__(
         self,
         loc,
@@ -11,6 +28,7 @@ class CustomGEV(tfd.GeneralizedExtremeValue):
         concentration,
         support_penalty: float = 1e6,
         extend_support_boundary_by: float = 0.1,
+        scale_penalty: bool = False,
         validate_args=False,
         allow_nan_stats=True,
         name="CustomGEV",
@@ -28,6 +46,7 @@ class CustomGEV(tfd.GeneralizedExtremeValue):
         self.gumbel_dist = tfd.Gumbel(loc=loc, scale=scale)
         self.support_penalty = support_penalty
         self.extend_support_boundary_by = extend_support_boundary_by
+        self.scale_penalty = scale_penalty
 
     @classmethod
     def _parameter_properties(cls, dtype, num_classes=None):
@@ -48,7 +67,7 @@ class CustomGEV(tfd.GeneralizedExtremeValue):
         # The values created here are only used to avoid NaNs;
         # they will be deselected by the final call to jnp.where.
         x_gumbel_safe = 0.5 * self.scale + self.loc
-        x_gev_safe = self.scale * (0.5 - 1.0) / self.concentration + self.loc
+        x_gev_safe = -0.5 * self.scale / self.concentration + self.loc
 
         safe_value = jnp.where(
             y <= 0,
@@ -69,12 +88,17 @@ class CustomGEV(tfd.GeneralizedExtremeValue):
         # the jnp.where.
         n_support_violations = jnp.max(jnp.array([(y <= 0).sum(), 1.0]))
 
+        if self.scale_penalty:
+            penalty = self.support_penalty / n_support_violations
+        else:
+            penalty = self.support_penalty
+
         # returns a penalized log prob, with the penalty getting stronger for stronger
         # deviations from the needed support.
         # say, y = -1, then we have -2 * support penalty
         # say, y = -0.1, then we have -1.1 * support penalty
         # The second case has a smaller penalty, which is what we want.
-        pseudo_lp = (y - 1.0) * (self.support_penalty / n_support_violations)
+        pseudo_lp = (y - 1.0) * penalty
 
         lp = jnp.where(y <= 0, pseudo_lp, log_prob)
 
