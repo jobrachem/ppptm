c91eadfaef6fee528c2d1dcbe06a4a446642a02a
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2025-10-17 14:25:06 +0200

    allow for turning off marginal model easily

--- src/ppptm/joint_model.py
+++ src/ppptm/joint_model.py
@@ -2,7 +2,8 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 import torch
-from batram.legmods import SimpleTM
+import veccs
+from batram.legmods import Data, SimpleTM
 from jax.typing import ArrayLike
 from scipy import stats
 
@@ -10,14 +11,67 @@ from .model import Model
 
 
 class CompositeTransformations:
-    def __init__(self, marginal: Model, dependence: SimpleTM):
+    def __init__(
+        self,
+        marginal: Model,
+        dependence: SimpleTM | None = None,
+        use_marginal: bool = True,
+    ):
         self.marginal = marginal
         self.dependence = dependence
+        self.use_marginal = use_marginal
+
+    @property
+    def locs(self) -> jax.Array:
+        return self.marginal.locs.ordered.value
+
+    @property
+    def y(self) -> jax.Array:
+        return self.marginal.response.value
+
+    def init_dependence_model(
+        self,
+        largest_conditioning_set: int = 30,
+        theta_init=None,
+        smooth=1.5,
+        nug_mult=4.0,
+    ) -> SimpleTM:
+        tloc = torch.tensor(self.locs)
+        nloc = self.locs.shape[0]
+        float_dtype = np.dtype(self.y.dtype)
+        int_dtype = np.dtype(jnp.array([1]).dtype)
+
+        nn, _ = veccs.preceding_neighbors(
+            coordinates=np.asarray(tloc, dtype=float_dtype),
+            sequence=np.arange(nloc, dtype=int_dtype),
+            num_neighbors=largest_conditioning_set,
+        )
+
+        nn = np.asarray(nn, dtype=int_dtype)
+
+        tnn = torch.as_tensor(nn)
+        train_hg = self.marginal.hg(self.y) if self.use_marginal else self.y
+
+        train_data = Data.new(tloc, torch.tensor(train_hg), tnn)
+
+        tm = SimpleTM(
+            train_data,
+            theta_init=theta_init,
+            linear=False,
+            smooth=smooth,
+            nug_mult=nug_mult,
+        )
+
+        return tm
 
     def log_score(self, obs: ArrayLike, logdet_addition: ArrayLike = 0.0) -> jax.Array:
         obs = jnp.asarray(obs)
-        mdist = self.marginal.init_dist()
-        obs_hg, obs_hg_logdet = mdist.transformation_and_logdet(obs)
+        if self.use_marginal:
+            mdist = self.marginal.init_dist()
+            obs_hg, obs_hg_logdet = mdist.transformation_and_logdet(obs)
+        else:
+            obs_hg = obs
+            obs_hg_logdet = jnp.asarray(0.0)
 
         with torch.no_grad():
             z, t_logdet = self.dependence.map_and_logdet(torch.tensor(obs_hg))
@@ -36,12 +90,12 @@ class CompositeTransformations:
         n: int,
         fixed_y: jax.typing.ArrayLike | None = None,
     ) -> jax.Array:
-        locs_model = self.marginal.locs.ordered.value
+        locs_model = self.locs
         z = jax.random.normal(key=key, shape=(n, locs_model.shape[0]))
 
         if fixed_y is None:
             fixed_zt = torch.tensor([])
-        else:
+        elif self.use_marginal:
             fixed_y = jnp.asarray(fixed_y)
             # marginal model expects data at all locations, so we pad the missing ones
             fixed_size = fixed_y.size
@@ -53,6 +107,8 @@ class CompositeTransformations:
             fixed_zt_ = self.marginal.hg(fy)[:, :fixed_size]
             # turn into tensor and remove leading axis
             fixed_zt = torch.as_tensor(np.asarray(fixed_zt_).copy()).squeeze(0)
+        else:
+            fixed_zt = torch.as_tensor(fixed_y)
 
         with torch.no_grad():
             zt = self.dependence.inverse_map(
